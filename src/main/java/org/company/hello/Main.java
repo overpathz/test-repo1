fd7bf9466785a214bb25334ff8c28a16a416d392
overpathz <overpathz@gmail.com>
2023-12-17 15:58:58 +0200

    add maven support

--- src/main/java/org/company/hello/Main.java
+++ src/main/java/org/company/hello/Main.java
@@ -1,5 +1,7 @@
 package org.company.hello;
 
+import org.postgresql.util.DriverInfo;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
